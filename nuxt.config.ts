254279d1df142c9450584a3d0a159e31c7f04df5
DawidKonkol <dawid.konkol@mghost.pl>
2024-03-01 10:44:16 +0100

    + important modules/packages, and initial scss file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 export default defineNuxtConfig({
   ssr: true,
   srcDir: 'src/',
+
+  css: [
+    'assets/scss/style.scss',
+  ],
+  modules: [ 
+    '@nuxtjs/seo',
+    '@nuxt/image',
+    'nuxt-delay-hydration'
+  ],
+
+
   devtools: { enabled: true }
 })
